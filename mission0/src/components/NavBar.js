55ee66dc3912dc00ac4a29fa5f0c83bf701ed1f1
Jorge Marquet <jorge@missionreadyhq.com>
2023-08-01 17:38:26 +1200

    worked exclusively on NavBar components

--- mission0/src/components/NavBar.js
+++ mission0/src/components/NavBar.js
@@ -3,8 +3,17 @@ import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   return (
-    <div id={styles.navBar}>
-      <div> NavBar </div>
+    <div id={styles.navBarContainer}>
+      <div id={styles.companyInfo}>
+        <img alt="Company Image"></img>
+        <p>My Company</p>
+      </div>
+      <ul id={styles.menuOptions}>
+        <li className={styles.menuOption}>menu 1</li>
+        <li className={styles.menuOption}>menu 2</li>
+        <li className={styles.menuOption}>menu 3</li>
+        <li className={styles.menuOption}>Login</li>
+      </ul>
     </div>
   );
 }
