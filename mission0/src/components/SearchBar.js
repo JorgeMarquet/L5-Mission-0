f09bac8ea29c95decd1543c940abd3abeb5c1324
Jorge Marquet <jorge@missionreadyhq.com>
2023-08-02 15:53:04 +1200

    modified top container and searchbar components. Added backgroundimage to topcontainer

--- mission0/src/components/SearchBar.js
+++ mission0/src/components/SearchBar.js
@@ -2,5 +2,10 @@ import React from "react";
 import styles from "./SearchBar.module.css";
 
 export default function SearchBar() {
-  return <div id={styles.searchBar}>SearchBar Test</div>;
+  return (
+    <div id={styles.searchBar}>
+      <input className={styles.inputBox} type="text"></input>
+      <button className={styles.searchButton}>search</button>
+    </div>
+  );
 }
--- mission0/src/components/TopContainer.js
+++ mission0/src/components/TopContainer.js
@@ -1,11 +1,16 @@
 import styles from "./TopContainer.module.css";
 import SearchBar from "./SearchBar";
+import backgroundImage from "../assets/logos/background_images/gray_background.png";
 
 export default function topContainer() {
   return (
     <div id={styles.topContainer}>
       <div>
-        Top Container
+        <img
+          className={styles.backgroundImage}
+          alt="background"
+          src={backgroundImage}
+        ></img>
         <SearchBar />
       </div>
     </div>
