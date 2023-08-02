3239b1daf198c6f82ed0e26114c9f1b026728b47
Jorge Marquet <jorge@missionreadyhq.com>
2023-08-02 15:07:00 +1200

    finalised first version of top navbar

--- mission0/src/components/NavBar.js
+++ mission0/src/components/NavBar.js
@@ -1,17 +1,22 @@
 import React from "react";
 import styles from "./NavBar.module.css";
+import mittens from "../assets/logos/mittens.png";
 
 export default function NavBar() {
   return (
     <div id={styles.navBarContainer}>
       <div id={styles.companyInfo}>
-        <img alt="Company Image"></img>
-        <p>My Company</p>
+        <img
+          className={styles.companyLogo}
+          src={mittens}
+          alt="Company Image"
+        ></img>
+        <p className={styles.companyName}>My Company</p>
       </div>
       <ul id={styles.menuOptions}>
-        <li className={styles.menuOption}>menu 1</li>
-        <li className={styles.menuOption}>menu 2</li>
-        <li className={styles.menuOption}>menu 3</li>
+        <li className={styles.menuOption}>Menu 1</li>
+        <li className={styles.menuOption}>Menu 2</li>
+        <li className={styles.menuOption}>Menu 3</li>
         <li className={styles.menuOption}>Login</li>
       </ul>
     </div>
