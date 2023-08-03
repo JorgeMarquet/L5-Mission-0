e20138537c067f68a24bfdcd8381be20fa956ac6
Jorge Marquet <jorge@missionreadyhq.com>
2023-08-03 16:56:06 +1200

    second version for show and tells but using State and Effect hooks instead of hardcoding each information card

--- mission0/src/components/BottomContainer.js
+++ mission0/src/components/BottomContainer.js
@@ -5,45 +5,8 @@ import catImage from "../assets/logos/mittens.png";
 export default function BottomContainer() {
   return (
     <div id={styles.bottomContainer}>
-      <div id={styles.informationCards}>
-        {/* <InformationCards /> */}
-
-        {/* Hardcoded cards */}
-        <div className={styles.informationCard}>
-          <img
-            src={catImage}
-            className={styles.informationCardImage}
-            alt="card image"
-          ></img>
-          <div className={styles.informationCardDetails}>
-            <h2>Name</h2>
-            <h4>Description</h4>
-          </div>
-        </div>
-
-        <div className={styles.informationCard}>
-          <img
-            src={catImage}
-            className={styles.informationCardImage}
-            alt="card image"
-          ></img>
-          <div className={styles.informationCardDetails}>
-            <h2>Name</h2>
-            <h4>Description</h4>
-          </div>
-        </div>
-
-        <div className={styles.informationCard}>
-          <img
-            src={catImage}
-            className={styles.informationCardImage}
-            alt="card image"
-          ></img>
-          <div className={styles.informationCardDetails}>
-            <h2>Name</h2>
-            <h4>Description</h4>
-          </div>
-        </div>
+      <div>
+        <InformationCards />
       </div>
     </div>
   );
--- mission0/src/components/InformationCards.js
+++ mission0/src/components/InformationCards.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./InformationCards.module.css";
+import infoData from "../assets/logos/cards_information/info.json"; // import the json file
 
 export default function InformationCards() {
-  return <div id={styles.informationCards}>InformationCards</div>;
+  const [infoCards, setInfoCards] = useState([]);
+
+  useEffect(() => {
+    setInfoCards(infoData.infoCards);
+  }, []);
+
+  return (
+    <div id={styles.informationCards}>
+      {infoCards.map(
+        (
+          card,
+          index // map through the data and generate the cards
+        ) => (
+          <div key={index} className={styles.informationCard}>
+            <img
+              className={styles.informationCardImage}
+              src={require(`../assets/logos/${card.photo}`)}
+              alt={card.Name}
+            ></img>
+            <h3>{card.Name}</h3>
+            <h4>{card.Description}</h4>
+          </div>
+        )
+      )}
+    </div>
+  );
 }
